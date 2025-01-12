e2dbc84fa7d342953f917b0134db8fb561b1743c
Alex Poputa <poputa.alex@gmail.com>
2025-01-12 11:53:51 +0200

    testing switch statement

--- global/types.ts
+++ global/types.ts
@@ -4,3 +4,11 @@ export type Contact = {
 	email: string;
 	phone: string;
 };
+
+export type ActionParams = {
+  action: string;
+  id?: string;
+  name?: string;
+  email?: string;
+  phone?: string;
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,51 @@
-import { listContacts,getContactById, removeContact,addContact } from "./contacts";
+import {
+	listContacts,
+	getContactById,
+	removeContact,
+	addContact,
+} from './contacts';
+import { ActionParams } from './global/types';
+import { program } from 'commander';
 
-// listContacts()
-// getContactById(4);
+
+// ;
 // removeContact(4);
-addContact('marcel','marcel@aslkfn.com',"98230293572309")
 
+
+program
+	.option('-a, --action <type>', 'choose action')
+	.option('-i, --id <type>', 'user id')
+	.option('-n, --name <type>', 'user name')
+	.option('-e, --email <type>', 'user email')
+	.option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts() as ActionParams;
+
+
+const invokeAction = ({ action, id, name = '', email = '', phone = '' }: ActionParams) => {
+    switch (action) {
+      case "list":
+        listContacts()
+        break;
+  
+      case "get":
+        getContactById(Number(id))
+        break;
+  
+      case "add":
+        addContact(name,email,phone)
+        break;
+  
+      case "remove":
+        removeContact(Number(id))
+        break;
+  
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  }
+
+  
+  invokeAction(argv);
